e24a9255bfb20282d3682031347b99db53f9e205
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-07-19 22:09:26 -0300

    Add files via upload

--- package_dados_CursoR.R
+++ package_dados_CursoR.R
@@ -39,3 +39,36 @@ ggplot(av2, aes(x = fabricante, y = media)) +
                     ymax = media + sd), width = 0.25, size = 0.8) +
   labs(x = "Fabricantes", y = "Número médio de assentos") +
   theme(axis.text = element_text(size = 8.5, color = "black"))
+
+av3 <- av1 %>%
+  group_by(tipo_motor) %>%
+  summarise(media = mean(assentos),
+            sd = sd(assentos),
+            n = n(),
+            se = sd/sqrt(n))
+av3
+
+ggplot(av3, aes(x = tipo_motor, y = media)) +
+  geom_col(fill = "#a6611a") +
+  geom_errorbar(aes(x = tipo_motor, y = media, ymin = media - sd,
+                    ymax = media + sd), width = 0.25, size = 0.8) +
+  labs(x = "Tipo de motor", y = "Número médio de assentos") +
+  theme(axis.text = element_text(size = 8.5, color = "black"))
+
+av4 <- av1 %>%
+  filter(ano != 1984) %>%
+  group_by(ano) %>%
+  summarise(media = mean(assentos),
+            sd = sd(assentos),
+            n = n(),
+            se = sd/sqrt(n)) %>%
+  drop_na()
+av4
+
+ggplot(av4, aes(x = ano, y = media, group = ano)) +
+  geom_point(color = "#a6611a", size = 5) +
+  geom_line(aes(x = ano, y = media, group = ano)) +
+  geom_errorbar(aes(x = ano, y = media, ymin = media - se,
+                    ymax = media + se), width = 0.25, size = 0.8) +
+  labs(x = "Tipo de motor", y = "Ano") +
+  theme(axis.text = element_text(size = 8.5, color = "black"))
